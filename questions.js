f9fa224b49744b48e4ad9dbe083440a79368089d
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-29 18:18:46 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The plane was delayed by the fog and took ___ at 10 instead of 9.",
+        options:["in", "out", "away", "off"],
+        answer: 3
+    },
+    {
+        q:"I'm worried how our daughter will take ___ secondary school next year.",
+        options:["to", "after", "out", "in"],
+        answer: 0
+    },
+    {
+        q:"If you don't take the books ___ to the library, they will fine you.",
+        options:["away", "back", "off", "down"],
+        answer: 1
+    },
+    {
+        q:"You'd better take ___ our phone number and don't hesitate to call us when you visit Paris.",
+        options:["on", "up", "down", "out"],
+        answer: 2
+    },
+    {
+        q:"We took ___ Rome on our way back from holyday.",
+        options:["up", "to", "out", "in"],
+        answer: 3
+    }
+]
